01a1d5c9a08a0a2dd703bd372f0a0f6233b4afe1
Stephen Haberman <stephen@exigencecorp.com>
2010-07-31 01:25:51 -0500

    Sync and upload based on exif and date_taken.

---
+++ sync.rb
@@ -0,0 +1,55 @@
+
+require 'rubygems'
+require 'flickraw'
+require 'sqlite3'
+
+module FlickRaw
+  FLICKR_HOST = 'www.23hq.com'
+end
+
+email = ARGV[0]
+password = ARGV[1]
+user_id = flickr.test.login(:email => email, :password => password)['id']
+
+should_create = ! File.exists?('sync.db')
+db = SQLite3::Database.new('sync.db')
+
+if should_create then
+  db.execute_batch <<SQL
+    CREATE TABLE photos (
+      id int,
+      date_taken varchar(50),
+      path varchar(200)
+   );
+SQL
+end
+
+page = 1
+while true do
+  puts "page #{page}"
+  photos = flickr.photos.search(
+                                :user_id => user_id,
+                                :extras => 'date_taken',
+                                :per_page => 500,
+                                :page => page,
+                                :email => email,
+                                :password => password
+                               )
+  photos.each do |photo|
+    count = db.get_first_value('SELECT count(*) FROM photos WHERE id = ?', photo['id'])
+    if count == '0' then
+      db.execute(
+                'INSERT INTO photos (id, date_taken, path) VALUES (?, ?, null);',
+                photo['id'],
+                photo['datetaken']
+                )
+    end
+  end
+  if photos.size == 500
+    page = page + 1
+  else
+    break
+  end
+end
+
+
---
+++ upload.rb
@@ -0,0 +1,54 @@
+
+require 'rubygems'
+require 'flickraw'
+require 'sqlite3'
+require 'rio'
+require 'mini_exiftool'
+
+module FlickRaw
+  FLICKR_HOST = 'www.23hq.com'
+end
+
+email = ARGV[0]
+password = ARGV[1]
+user_id = flickr.test.login(:email => email, :password => password)['id']
+
+db = SQLite3::Database.new('sync.db')
+
+rio('/home/stephen/Pictures/Photos').all.files.each do |file|
+  path = file.path
+  is_modified = path.include? '(Modified)'
+  has_modified = (!is_modified) && File.exists?(path.gsub('.', ' (Modified).'))
+  if is_modified or !has_modified then
+    exif = MiniExiftool.new path
+    date_taken = exif['DateTimeOriginal'].strftime '%Y-%m-%d %H:%M:%S'
+    tags = exif['Subject'].to_a.join ' '
+
+    # Does our database think we've uploaded this before?
+    count = db.get_first_value('SELECT count(*) FROM photos WHERE date_taken = ?', date_taken)
+    if count != '0' then
+      next
+    end
+
+    # Does 23hq think we've uploaded this before?
+    photos = flickr.photos.search(
+                                  :user_id => user_id,
+                                  :email => email,
+                                  :password => password,
+                                  :max_taken_date => date_taken,
+                                  :min_taken_date => date_taken
+                                 )
+    if photos.total > 0 then
+      puts "Already uploaded #{path}"
+      db.execute 'INSERT INTO photos (id, date_taken) VALUES (?, ?)', photos[0]['id'], date_taken
+      next
+    end
+
+    if count == '0' then
+      puts "Uploading #{path} #{tags}"
+      p = flickr.upload_photo path, :tags => tags, :email => email, :password => password, :is_public => 1, :hidden => 2
+      # db.execute 'INSERT INTO photos (id, date_taken) VALUES (?, ?)', p, date_taken
+    end
+  end
+end
+
